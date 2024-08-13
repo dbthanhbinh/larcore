3bee82fc6c346cd977a45a8800cfef006156ede0
Binh Trinh <bintrinh@deloitte.ca>
2024-08-13 14:54:53 +0700

    Init: Add reactjs ts front-end project

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,14 @@ import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
+        // laravel({
+        //     input: ['resources/css/app.css', 'resources/js/app.js'],
+        //     refresh: true,
+        // }),
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/myapp/src/main.tsx'],
             refresh: true,
         }),
+        react(),
     ],
 });
